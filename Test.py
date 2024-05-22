b063dd34b743c99b63d459dc0ea283484c2fec7b
ArtBelqa <ArtBel.qa@gmail.com>
2024-05-22 21:43:11 +1000

    1

--- Test.py
+++ Test.py
@@ -7,17 +7,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Инициализация веб-драйвера
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 try:
     driver.maximize_window()
-
-    # Открыть сайт
     driver.get("https://www.roseltorg.ru/")
 
-    # Явное ожидание, чтобы элемент стал кликабельным
-    wait = WebDriverWait(driver, 3)
+    wait = WebDriverWait(driver, 5)
 
     # Перейти в раздел 44-ФЗ
     section_44fz = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.menu__link[href="/search/44fz"]')))
@@ -28,31 +24,25 @@ try:
     search_field.send_keys("0372200286524000027")
     search_field.send_keys(Keys.RETURN)
 
-    # Подождать пока загрузится страница с результатами поиска
     time.sleep(2)
 
     # Перейти на страницу с тендером 0372200286524000027
-    tender_link = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.search-results__link[href="/procedure/0372200286524000027"]')))
+    tender_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.search-results__link[href="/procedure/0372200286524000027"]')))
     tender_link.click()
 
-    # Подождать пока загрузится страница с результатами поиска
     time.sleep(3)
 
     # Нажать на кнопку «Подать заявку»
     apply_button = driver.find_element(By.CSS_SELECTOR,'a.button.button--large.button--blue.button--main-action.procedura-podat-zayavku')
     apply_button.click()
 
-    # Подождать пока загрузится страница с информацией-предупреждением
-    time.sleep(3)
+    time.sleep(5)
 
     # Нажать на кнопку «Продолжить работу»
-    continue_button = driver.find_element(By.LINK_TEXT, "Продолжить работу")
+    continue_button = driver.find_element(By.CSS_SELECTOR,'a.button.button--blue[href="https://gos.roseltorg.ru/#procedures"]')
     continue_button.click()
 
-    # Подождать пока загрузится страница авторизации
-    time.sleep(60)
+    time.sleep(10)
 
 finally:
-    # Закрыть браузер
     driver.quit()
